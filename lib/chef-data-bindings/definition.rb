06f083942e3bd360d752a6ee7db4f040235567f8
danielsdeleo <dan@opscode.com>
2013-04-28 16:17:36 -0700

    extract bind types to their own file

--- lib/chef-data-bindings/definition.rb
+++ lib/chef-data-bindings/definition.rb
@@ -1,60 +1,6 @@
+require 'chef-data-bindings/bind_types'
 module ChefDataBindings
   module Definition
-
-    module BindTypes
-      class Static
-        def initialize(name, value)
-          @name = name
-          @value = value
-        end
-
-        def call(context_object)
-          @value
-        end
-      end
-
-      class Lambda
-        def initialize(name, block)
-          @name = name
-          @block = block
-        end
-
-        def call(context_object)
-          @block.call
-        end
-      end
-
-      class AttributePath
-
-        attr_reader :name
-
-        class MissingNodeAttribute < StandardError
-        end
-
-        def initialize(name, *path_specs)
-          @name = name
-          @path_specs = path_specs
-        end
-
-        def call(context_object)
-          searched_path = []
-          @path_specs.inject(context_object.node) do |attrs_so_far, attr_key|
-            searched_path << attr_key
-            next_attrs = attrs_so_far[attr_key]
-            if !next_attrs.nil?
-              next_attrs
-            else
-              current_lookup = "node[:#{searched_path.join('][:')}]"
-              desired_lookup = "node[:#{@path_specs.join('][:')}]"
-              raise MissingNodeAttribute,
-               "Error finding value `#{name}' from attributes `#{desired_lookup}`: `#{current_lookup}` is nil"
-            end
-          end
-        end
-      end
-
-
-    end
 
     class DataBindingDefinition
 
